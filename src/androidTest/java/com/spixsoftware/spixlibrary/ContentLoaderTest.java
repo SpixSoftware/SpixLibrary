c8f4cc4312b045643f4aead49534a305fa7cf69a
AndrejLukasevic <hexagonx@gmail.com>
2014-12-22 23:02:17 +0200

    more tests added

--- src/androidTest/java/com/spixsoftware/spixlibrary/ContentLoaderTest.java
+++ src/androidTest/java/com/spixsoftware/spixlibrary/ContentLoaderTest.java
@@ -6,6 +6,8 @@ import android.test.ApplicationTestCase;
 import com.spixsoftware.spixlibrary.tools.contentpreparer.ContentLoader;
 import com.spixsoftware.spixlibrary.tools.contentpreparer.ContentPreparer;
 
+import java.util.Random;
+
 public class ContentLoaderTest extends ApplicationTestCase<Application> {
 
 
@@ -37,6 +39,34 @@ public class ContentLoaderTest extends ApplicationTestCase<Application> {
         assertEquals("result", result);
     }
 
+    public void testGetContentMultiple() {
+
+        for (int i = 0; i < 1000; i++) {
+            ContentPreparer<String, String> contentPreparer = new ContentPreparer<>();
+
+            String result = contentPreparer.getContent("" + i, new ContentLoader<String, String>() {
+                @Override
+                public String loadContent(String s) {
+                    try {
+                        Thread.sleep(randInt(0, 50));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return "result";
+                }
+
+                @Override
+                public String prepareArguments() {
+                    return "prepared";
+                }
+            });
+            assertEquals("result", result);
+
+        }
+
+
+    }
+
     public void testGetPreparedContent() {
         ContentPreparer<String, String> contentPreparer = new ContentPreparer<>();
 
@@ -62,6 +92,43 @@ public class ContentLoaderTest extends ApplicationTestCase<Application> {
         String result = contentPreparer.getContent("1", loader);
 
         assertEquals("result", result);
+    }
+
+    public void testGetPreparedContentMultiple() {
+
+        for (int i = 0; i < 1000; i++) {
+
+            ContentPreparer<String, String> contentPreparer = new ContentPreparer<>();
+
+            final ContentLoader<String, String> loader = new ContentLoader<String, String>() {
+                @Override
+                public String loadContent(String s) {
+                    try {
+                        Thread.sleep(randInt(0, 50));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return "result";
+                }
+
+                @Override
+                public String prepareArguments() {
+                    return "prepared";
+                }
+            };
+
+            contentPreparer.prepareContent("" + i, loader);
+
+            try {
+                Thread.sleep(randInt(0, 50));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            String result = contentPreparer.getContent("" + i, loader);
+
+            assertEquals("result", result);
+        }
     }
 
     public void testGetPreparedContent2() {
@@ -112,6 +179,12 @@ public class ContentLoaderTest extends ApplicationTestCase<Application> {
         String result = contentPreparer.getContent("1", loader);
 
         assertEquals("result", result);
+    }
+
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
     }
 
 }
